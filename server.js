2e4422ef5338e0305b6ef4eda727670836f1b22e
Liscp <lcp.caiza@yavirac.edu.ec>
2020-02-12 22:02:58 -0500

    ultimas_versiones

--- server.js
+++ server.js
@@ -1,6 +1,47 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
+const decoryDuck = require('./obj/decoryDuck');
+const mallarDuck = require('./obj/mallarDuck');
+const redHeadDuck = require('./obj/redHeadDuck');
+const rubberDuck = require('./obj/rubberDuck');
+const app = express();
+const pato = express.Router();
 const port = process.env.PORT || 3000;
-const server = http.createServer(app);
 
-server.listen(port);
+pato.route("/duck/:id")
+    .get((req,res) => {
+        const my= req.params.id
+    console.log(typeof(my))
+    const mallar= new mallarDuck('Soy pepe Mallar');
+    const decory= new decoryDuck('Soy de Decoracion')
+    const red= new redHeadDuck('Soy de cabeza roja')
+    const rubbert= new rubberDuck('Soy rubbert')
+const objetos={
+    mallarobj:{
+        params: mallar.display,
+        params2: mallar.quack(),
+        params3: mallar.fly()
+    },
+    decoryobj:{
+        params: decory.display
+    },
+    
+   redobj:{
+        params: red.display,
+        params2:red.fly(),
+        params3: red.quack()
+    },
+    
+   rubbertobj :{
+        params: rubbert.display,
+        params2: rubbert.quack()
+    }
+}
+
+    res.json(Object.values(objetos[`${my}obj`]))
+    });
+
+app.use(pato)
+
+app.listen(port, ()=>{
+    console.log(`Runing on port ${port}`);
+});
